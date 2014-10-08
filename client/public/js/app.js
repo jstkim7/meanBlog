1a0f775fd184aaa6369c0d1dd0c04feab82209f8
Sung Kim <jstkim7@gmail.com>
2014-10-08 18:24:33 -0400

    Basic routing

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -1,1 +1,14 @@
-angular.module("meanBlog", []);
+angular.module('meanBlog', ['ngRoute'])
+
+.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+    .when('/', {
+      templateUrl: 'views/articles/articles.html'
+    });
+
+    $locationProvider.html5Mode(true);
+  }]);
+
