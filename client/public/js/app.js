e5b382773050c340fbd462b759a201fc22629c90
Sung Kim <jstkim7@gmail.com>
2014-10-12 11:10:13 -0400

    Create basic routing for angular

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -7,7 +7,19 @@ angular.module('meanBlog', ['ngRoute'])
 
     .when('/', {
       templateUrl: 'views/articles/articles.html'
-    });
+    })
+
+    .when('/articles/:article_id', {
+      templateUrl: 'views/articles/article.html'
+    })
+
+    .when('/articles/new', {
+      templateUrl: 'views/articles/articleForm.html'
+    })
+
+    .when('/articles/edit', {
+      templateUrl: 'views/articles/articleForm.html'
+    })
 
     $locationProvider.html5Mode(true);
   }]);
